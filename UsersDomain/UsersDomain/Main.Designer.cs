67bdaf751d8ee3570c4c959c3dd4b2887569cd02
GrNik <gr_nik@mail.ru>
2012-05-04 17:50:46 +0400

    Start project

---
+++ UsersDomain/UsersDomain/Main.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace UsersDomain
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCheckUser = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btAllUser = new System.Windows.Forms.Button();
+            this.txtDomainPath = new System.Windows.Forms.TextBox();
+            this.dtgAllUsers = new System.Windows.Forms.DataGridView();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btAddUser = new System.Windows.Forms.Button();
+            this.dtgRoles = new System.Windows.Forms.DataGridView();
+            this.btGetRoles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAllUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgRoles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btCheckUser
+            // 
+            this.btCheckUser.Location = new System.Drawing.Point(12, 12);
+            this.btCheckUser.Name = "btCheckUser";
+            this.btCheckUser.Size = new System.Drawing.Size(75, 23);
+            this.btCheckUser.TabIndex = 0;
+            this.btCheckUser.Text = "Check user";
+            this.btCheckUser.UseVisualStyleBackColor = true;
+            this.btCheckUser.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(151, 12);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // btAllUser
+            // 
+            this.btAllUser.Location = new System.Drawing.Point(12, 70);
+            this.btAllUser.Name = "btAllUser";
+            this.btAllUser.Size = new System.Drawing.Size(75, 23);
+            this.btAllUser.TabIndex = 2;
+            this.btAllUser.Text = "All user";
+            this.btAllUser.UseVisualStyleBackColor = true;
+            this.btAllUser.Click += new System.EventHandler(this.btAllUser_Click);
+            // 
+            // txtDomainPath
+            // 
+            this.txtDomainPath.Location = new System.Drawing.Point(151, 72);
+            this.txtDomainPath.Name = "txtDomainPath";
+            this.txtDomainPath.Size = new System.Drawing.Size(100, 20);
+            this.txtDomainPath.TabIndex = 4;
+            // 
+            // dtgAllUsers
+            // 
+            this.dtgAllUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgAllUsers.Location = new System.Drawing.Point(12, 99);
+            this.dtgAllUsers.Name = "dtgAllUsers";
+            this.dtgAllUsers.Size = new System.Drawing.Size(240, 177);
+            this.dtgAllUsers.TabIndex = 6;
+            // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(12, 41);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(75, 23);
+            this.btSearch.TabIndex = 7;
+            this.btSearch.Text = "Search";
+            this.btSearch.UseVisualStyleBackColor = true;
+            this.btSearch.Click += new System.EventHandler(this.btSearch_Click);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(151, 41);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(100, 20);
+            this.txtFilter.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Roles";
+            // 
+            // btAddUser
+            // 
+            this.btAddUser.Location = new System.Drawing.Point(177, 431);
+            this.btAddUser.Name = "btAddUser";
+            this.btAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btAddUser.TabIndex = 13;
+            this.btAddUser.Text = "Add user";
+            this.btAddUser.UseVisualStyleBackColor = true;
+            // 
+            // dtgRoles
+            // 
+            this.dtgRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgRoles.Location = new System.Drawing.Point(12, 310);
+            this.dtgRoles.Name = "dtgRoles";
+            this.dtgRoles.Size = new System.Drawing.Size(240, 109);
+            this.dtgRoles.TabIndex = 14;
+            // 
+            // btGetRoles
+            // 
+            this.btGetRoles.Location = new System.Drawing.Point(14, 431);
+            this.btGetRoles.Name = "btGetRoles";
+            this.btGetRoles.Size = new System.Drawing.Size(75, 23);
+            this.btGetRoles.TabIndex = 15;
+            this.btGetRoles.Text = "GetRoles";
+            this.btGetRoles.UseVisualStyleBackColor = true;
+            this.btGetRoles.Click += new System.EventHandler(this.btGetRoles_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Filter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Domain";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 463);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btGetRoles);
+            this.Controls.Add(this.dtgRoles);
+            this.Controls.Add(this.btAddUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.btSearch);
+            this.Controls.Add(this.dtgAllUsers);
+            this.Controls.Add(this.txtDomainPath);
+            this.Controls.Add(this.btAllUser);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btCheckUser);
+            this.Name = "Main";
+            this.Text = "Manager user DB";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAllUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgRoles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCheckUser;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btAllUser;
+        private System.Windows.Forms.TextBox txtDomainPath;
+        private System.Windows.Forms.DataGridView dtgAllUsers;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btAddUser;
+        private System.Windows.Forms.DataGridView dtgRoles;
+        private System.Windows.Forms.Button btGetRoles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ UsersDomain/UsersDomain/Main.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.Data.SqlClient;
+
+namespace UsersDomain
+{
+    //http://www.codeproject.com/Articles/9570/Querying-Microsoft-Active-Directory-Using-Microsof
+    //http://msdn.microsoft.com/en-us/library/system.directoryservices.directorysearcher.findall.aspx
+
+    /*
+CREATE LOGIN "PNG\GazeevAP" FROM WINDOWS;
+CREATE USER "PNG\GazeevAP";
+EXEC sp_addrolemember 'db_datareader', "PNG\GazeevAP"
+DROP USER "PNG\GazeevAP";
+DROP LOGIN "PNG\GazeevAP";
+    */
+
+    public partial class Main : Form
+    {
+        private string _connectionString;
+
+        sealed class DomainUser
+        {
+            public string Account { get; private set; }
+            public string Name { get; private set; }
+
+            public DomainUser(string account, string name)
+            {
+                Account = account;
+                Name = name;
+            }
+
+            public DomainUser(string account)
+                : this(account, "")
+            {
+            }
+        }
+
+        sealed class RoleDB
+        {
+            public string Role { get; private set; }
+            public string Description { get; private set; }
+
+            public RoleDB(string role, string desciption)
+            {
+                Role = role;
+                Description = desciption;
+            }
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+
+            _connectionString = Properties.Settings.Default.ConnectionString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(IsExistInAD(txtUserName.Text).ToString());
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            BindingSource bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = SearchInAD(txtFilter.Text);
+
+            dtgAllUsers.DataSource = bindingSource1;
+        }
+
+        private void btAllUser_Click(object sender, EventArgs e)
+        {
+            BindingSource bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = GetAllADDomainUsers(txtDomainPath.Text);
+
+            dtgAllUsers.DataSource = bindingSource1;
+        }
+
+        #region Function
+
+        string ExtractUserName(string path)
+        {
+            string[] userPath = path.Split(new char[] { '\\' });
+            return userPath[userPath.Length - 1];
+        }
+
+        bool IsExistInAD(string loginName)
+        {
+            string userName = ExtractUserName(loginName);
+            DirectorySearcher search = new DirectorySearcher();
+            search.Filter = String.Format("(SAMAccountName={0})", userName);
+            search.PropertiesToLoad.Add("cn");
+            SearchResult result = search.FindOne();
+
+            if (result == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        List<DomainUser> SearchInAD(string filter)
+        {
+            List<DomainUser> findUsers = new List<DomainUser>();
+
+            string userName = ExtractUserName(filter);
+            DirectorySearcher search = new DirectorySearcher();
+            search.Filter = String.Format("(SAMAccountName={0})", userName);
+            search.PropertiesToLoad.Add("displayName");                             // name
+            search.PropertiesToLoad.Add("samaccountname");                             // account
+            search.PropertiesToLoad.Add("cn");
+            SearchResult result;
+            using (SearchResultCollection resultCol = search.FindAll())
+            {
+                for (int counter = 0; counter < resultCol.Count; counter++)
+                {
+                    result = resultCol[counter];
+                    if (result.Properties.Contains("samaccountname"))
+                    {
+                        if (result.Properties.Contains("displayName"))
+                            findUsers.Add(new DomainUser((String)result.Properties["samaccountname"][0],
+                                                         (String)result.Properties["displayName"][0]));
+                        else
+                            findUsers.Add(new DomainUser((String)result.Properties["samaccountname"][0]));
+                    }
+                }
+                resultCol.Dispose();
+            }
+            return findUsers;
+        }
+
+        List<DomainUser> GetAllADDomainUsers(string domainpath)
+        {
+            List<DomainUser> allUsers = new List<DomainUser>();
+
+            DirectoryEntry searchRoot = new DirectoryEntry(domainpath);
+            DirectorySearcher search = new DirectorySearcher(searchRoot);
+            search.Filter = "(&(objectClass=user)(objectCategory=person))";
+            search.PropertiesToLoad.Add("displayName");                             // name
+            search.PropertiesToLoad.Add("samaccountname");
+
+            SearchResult result;
+            using (SearchResultCollection resultCol = search.FindAll())
+            {
+                if (resultCol != null)
+                {
+                    for (int counter = 0; counter < resultCol.Count; counter++)
+                    {
+                        result = resultCol[counter];
+                        if (result.Properties.Contains("samaccountname"))
+                        {
+                            if (result.Properties.Contains("displayName"))
+                                allUsers.Add(new DomainUser((String)result.Properties["samaccountname"][0], (String)result.Properties["displayName"][0]));
+                            else
+                                allUsers.Add(new DomainUser((String)result.Properties["samaccountname"][0]));
+                        }
+                    }
+                }
+                resultCol.Dispose();
+            }
+            return allUsers;
+        }
+
+        List<RoleDB> GetRolesDB()
+        {
+            List<RoleDB> result = new List<RoleDB>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                SqlCommand comm = new SqlCommand(@"EXEC sp_helpdbfixedrole;", conn);
+                SqlDataReader reader = comm.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.Add(new RoleDB(reader[0].ToString(), reader[1].ToString()));
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        private void btGetRoles_Click(object sender, EventArgs e)
+        {
+            dtgRoles.DataSource = GetRolesDB();
+        }
+    }
+}
